8dab574f02c7653dbf1bccd967240a50e018cbaf
express <express@DESKTOP-8VSS47T>
2023-09-09 01:43:32 +0200

    Add project files.

---
+++ WebApplication3/Controllers/recipesController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private object recipe;
+
+      //  public object?[]? Name { get; private set; }
+
+        public RecipesController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Recipes>>> Get()
+        {
+            return Ok(await _context.Allrecipes.ToListAsync());
+        }
+
+        [HttpGet("{Name}")]
+        
+        public async Task<ActionResult<Recipes>> Get(string Name)
+        {
+            var recipe = await _context.Allrecipes.FindAsync(Name);
+            if (recipe != null)
+                return Ok(recipe);
+            return BadRequest("Hero not found.");
+        }
+
+
+        //  public List <Recipes> Search  (string Name)
+        //.Where(a => a.Name == Name).ToList();
+        
+
+        [HttpPost]
+        public async Task<ActionResult<List<Recipes>>> AddHero(Recipes recipe)
+        {
+            _context.Allrecipes.Add(recipe);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Allrecipes.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Recipes>>> UpdateHero(Recipes request)
+        {
+            var dbrecipe = await _context.Allrecipes.FindAsync(request.Name);
+            if (dbrecipe == null)
+                return BadRequest("Hero not found.");
+
+            dbrecipe.Name = request.Name;
+            dbrecipe.Ingredients = request.Ingredients;
+            dbrecipe.Steps = request.Steps;
+            dbrecipe.Category = request.Category;
+
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Allrecipes.ToListAsync());
+        }
+        [HttpDelete("{Name}")]
+        public async Task<ActionResult<List<Recipes>>> Delete(string Name)
+        {
+            var dbrecipe = await _context.Allrecipes.FindAsync(Name);
+            if (dbrecipe == null)
+                return BadRequest("Hero not found.");
+
+            _context.Allrecipes.Remove(dbrecipe);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Allrecipes.ToListAsync());
+        }
+
+
+
+    }
+}
---
+++ WebApplication3/recipes.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication3
+{
+    
+    public class Recipes
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Ingredients { get; set; }= string.Empty;
+        public string Steps { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+
+
+
+    }
+
+}
